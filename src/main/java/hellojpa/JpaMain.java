67c0600e68f1fb1921893d2a12d1becbc97c0796
awbwd4 <awbwd4@gmail.com>
2022-06-21 19:56:11 +0900

    22.06.21 flush

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -19,13 +19,27 @@ public class JpaMain {
 
         try {
             //영속
-            Member findMember = em.find(Member.class, 150L);
-            findMember.setName("ZZZZ");
+            Member memberA = new Member(201L, "member201");
+            Member memberB = new Member(202L, "member202");
+            Member memberC = new Member(203L, "member203");
+            em.persist(memberA);
+            em.persist(memberB);
+            em.persist(memberC);
+
+            List<Member> members = em.createQuery("select m from Member m", Member.class).getResultList();
+
+            for (Member member : members ) {
+                System.out.println("member.getName() = " + member.getName());
+            }
+//            em.flush(); // 이 시점에 DB로 인서트 즉시 시행
+
             System.out.println("=================");
             tx.commit();
         } catch (Exception e) {
+            System.out.println("catch");
             tx.rollback();
         }finally {
+            System.out.println("finally");
             em.close();
         }
         emf.close();
