2b7fcd8ed934d85e2288932d032d31b57ed45c75
awbwd4 <awbwd4@gmail.com>
2022-06-21 19:12:31 +0900

    22.06.21 jpa entity insert

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -18,27 +18,16 @@ public class JpaMain {
         tx.begin();
 
         try {
-            // 비영속
-            Member member = new Member();
-            member.setId(101L);
-            member.setName("HelloJPA");
-
             //영속
-//            System.out.println("===before===");
-//            em.persist(member); // 1차 캐시에 저장.
-//            System.out.println("===after===");
+            Member member1 = new Member(150L, "A");
+            Member member2 = new Member(160L, "B");
 
-//            Member findMember = em.find(Member.class, 101L); // 1차 캐시에 있는걸 조회.
-
-            Member findMember1 = em.find(Member.class, 101L); // DB에서 조회
-            Member findMember2 = em.find(Member.class, 101L); // 1차 캐시에 있는걸 조회.
-
-
-            System.out.println("result = "+(findMember1 == findMember2));
-//            System.out.println("findMember.getId() = " + findMember.getId());
-//            System.out.println("findMember.getName() = " + findMember.getName());
+            em.persist(member1);
+            em.persist(member2);
+            System.out.println("====================");
 
             tx.commit();
+
         } catch (Exception e) {
             tx.rollback();
         }finally {
