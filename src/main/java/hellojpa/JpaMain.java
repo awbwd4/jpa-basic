ca1af91569dbd29fe3eb3f77c0a17d099fec7e6d
awbwd4 <awbwd4@gmail.com>
2022-06-22 15:07:24 +0900

    22.06.22 sequence generatot

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -19,11 +19,11 @@ public class JpaMain {
 
         try {
             Member member = new Member();
-            member.setId(3L);
+//            member.setId("ID_A");
             member.setUsername("C");
-            member.setRoleType(RoleType.GUEST);
 
             em.persist(member);
+
 
             tx.commit();
         } catch (Exception e) {
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -6,44 +6,25 @@ import java.time.LocalDateTime;
 import java.util.Date;
 
 @Entity//JPA가 관리하는 객체.
+@SequenceGenerator(//테이블마다 시퀀스 따로 관리
+        name="MEMBER_SEQ_GENERATOR",
+        sequenceName = "MEMBER_SEQ", //매핑할 데이터베이스 시퀀스 이름
+        initialValue = 1,
+        allocationSize = 10 //시퀀스 한번에 증가하는 수
+        )
 public class Member {
 
     @Id //pk
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+                    generator = "MEMBER_SEQ_GENERATOR")
+    private Long id; //int는 10억이 넘어가면 다시 0부터 시작됨 id 같은건 그냥 Long쓰는게 나음
+    //IDENTITY : 기본키 생성을 DB에 위임. flush되는 sql을 보면 id 값이 null이다
 
-    @Column(name = "name", nullable = false, unique = true)//테이블 칼럼명
+
+    @Column(name = "name", nullable = false)//테이블 칼럼명
     private String username;
 
-    private Integer age;
-
-//    @Enumerated(EnumType.STRING)//ENUM타입
-    @Enumerated(EnumType.STRING)//ENUM은 반드시 STRING이어야함
-    private RoleType roleType;//USER, ADMIN
-
-    @Temporal(TemporalType.TIMESTAMP) //DB와 date 타입을 맞춤
-    private Date createDate;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastModifiedDate;
-
-    private LocalDate testLocalDate;
-    private LocalDateTime testLocalDateTime;
-
-    @Lob // varchar를 넘어가는 길이의 데이터
-    private String descrition;
-
-    @Transient //DB와 연동하지 않음
-    private int temp;
-
     public Member() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getUsername() {
@@ -52,53 +33,5 @@ public class Member {
 
     public void setUsername(String username) {
         this.username = username;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public RoleType getRoleType() {
-        return roleType;
-    }
-
-    public void setRoleType(RoleType roleType) {
-        this.roleType = roleType;
-    }
-
-    public Date getCreateDate() {
-        return createDate;
-    }
-
-    public void setCreateDate(Date createDate) {
-        this.createDate = createDate;
-    }
-
-    public Date getLastModifiedDate() {
-        return lastModifiedDate;
-    }
-
-    public void setLastModifiedDate(Date lastModifiedDate) {
-        this.lastModifiedDate = lastModifiedDate;
-    }
-
-    public String getDescrition() {
-        return descrition;
-    }
-
-    public void setDescrition(String descrition) {
-        this.descrition = descrition;
-    }
-
-    public int getTemp() {
-        return temp;
-    }
-
-    public void setTemp(int temp) {
-        this.temp = temp;
     }
 }
