9a3be6d5a69963a1ea4ec0d816f0f274066464c8
awbwd4 <awbwd4@gmail.com>
2022-06-26 21:29:53 +0900

    22.06.26 Proxy -2 getReference()

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,9 +1,8 @@
 package hellojpa;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -22,61 +21,28 @@ public class JpaMain {
 
             Member member = new Member();
             member.setUsername("user");
-
-            Member member1 = new Member();
-            member.setUsername("user1");
-
-            Member member2 = new Member();
-            member.setUsername("user2");
-
             em.persist(member);
-            em.persist(member1);
-            em.persist(member2);
 
             em.flush();
             em.clear();
 
-//            Member m1 = em.find(Member.class, member1.getId());
-//
-//            Member m2 = em.getReference(Member.class, member2.getId());
-//
-//            System.out.println("m1 == m2 :  " + (m1.getClass()==m2.getClass()));
-//            System.out.println("m1 == m2 :  " + (m1 instanceof Member));
-//            System.out.println("m1 == m2 :  " + (m2 instanceof Member));
+            Member refMember = em.getReference(Member.class, member.getId());
+            System.out.println("refMember.getClass() = " + refMember.getClass());
 
-            Member reference = em.getReference(Member.class, member1.getId());
-//            System.out.println("reference.getClass() = " + reference.getClass());
-//            //동일한 영속성 컨텍스트 안에서는 getReference를 해도 1차 캐시에 이미 있는 실제 엔티티를 가져옴
-//            System.out.println("a ==== a : "+(m1 == reference));
-//            System.out.println("m1.class() : "+ m1.getClass());
-//            System.out.println("reference.class() : "+ reference.getClass());
+//            refMember.getUsername(); //JPA식 강제 초기화,jpa에는 강제 초기화를 위한 특정한 기능이 없음.
 
-            em.detach(reference);
+            Hibernate.initialize(refMember);//강제초기화
+
+
+//            em.detach(refMember);
 //            em.close();
-
-            System.out.println("reference.getUsername()    :    "+reference.getUsername());
-
-
-            System.out.println("/////////////////조회//////////////////");
+            // 영속성 컨텍스트로 관리가 되지 않으면 프록시 초기화 불가
 
 
-//            Member findMember = em.find(Member.class, member.getId());
-            Member findMeberR = em.getReference(Member.class, member.getId());//프록시 객체
-//            System.out.println("findMember.getId() = " + findMember.getId());
-//            System.out.println("findMember.getUsername() = " + findMember.getUsername());
-            System.out.println("findMeberR = " + findMeberR.getClass());
-            System.out.println("findMeberR.getId() = " + findMeberR.getId());
+            //프록시 초기화 여부 확인
+//            System.out.println("isLoaded = "+ emf.getPersistenceUnitUtil().isLoaded(refMember));
 
-            //여기에서 실제 엔티티 생성및 초기화됨
-            System.out.println("findMeberR.getUsername() = " + findMeberR.getUsername());
-            // 두번째부터는 프록시에 값이 있으므로 초기화 생성x
-            System.out.println("findMeberR.getUsername() = " + findMeberR.getUsername());
-            System.out.println("findMeberR = " + findMeberR.getClass());
-            // 초기화는 됐지만 여전히 프록시는 프록시다 실제 엔티티 아님. 걍 프록시 내 값이 채워지는것.
 
-            
-            
-            
             tx.commit();
         } catch (Exception e) {
             System.out.println("catch");
